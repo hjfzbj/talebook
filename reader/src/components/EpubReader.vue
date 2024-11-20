2e489013445ae2a4c087723c74340427ce1ff6db
Rex <talebook@foxmail.com>
2024-11-21 00:02:30 +0800

    调试了几天，原来npm里的epubjs版本有问题，真是太坑了！更换为0.3.88版本，一切正常了(宽屏页面的click还得调试下)

--- reader/src/components/EpubReader.vue
+++ reader/src/components/EpubReader.vue
@@ -2,36 +2,19 @@
   <v-app full-height>
     <!-- 顶部菜单 -->
     <v-app-bar v-if="active_menu" density="compact" color="primary">
-      <template v-slot:prepend>
-        <v-btn icon> <v-icon>mdi-arrow-left</v-icon> </v-btn>
-      </template>
-      <v-app-bar-title>{{ book_title }}</v-app-bar-title>
+      <template v-slot:prepend> <v-btn icon> <v-icon>mdi-arrow-left</v-icon> </v-btn> </template>
+      {{alert_msg}}
       <v-spacer></v-spacer>
       <v-btn icon> <v-icon>mdi-dots-vertical</v-icon> </v-btn>
     </v-app-bar>
 
 
     <!-- 底部菜单 -->
-    <v-bottom-navigation absolute :active="active_menu" color="primary" z-index="2599">
-      <v-btn value="favorites" @click="show_toc()">
-        <v-icon>mdi-book-open-variant-outline</v-icon>
-        <span>目录</span>
-      </v-btn>
-
-      <v-btn value="theme" @click="switch_theme">
-        <v-icon>{{ switch_theme_icon }}</v-icon>
-        <span>{{ switch_theme_text }}</span>
-      </v-btn>
-
-      <v-btn value="settings" @click="show_settings()">
-        <v-icon>mdi-cog</v-icon>
-        <span>设置</span>
-      </v-btn>
-
-      <v-btn value="next" @click='rendition.next()'>
-        <v-icon>mdi-dots-horizontal-circle-outline</v-icon>
-        <span>更多</span>
-      </v-btn>
+    <v-bottom-navigation :active="active_menu" color="primary" z-index="2599">
+      <v-btn value="favorites" @click="show_toc()"> <v-icon>mdi-book-open-variant-outline</v-icon> <span>目录</span> </v-btn>
+      <v-btn value="theme" @click="switch_theme"> <v-icon>{{ switch_theme_icon }}</v-icon> <span>{{ switch_theme_text }}</span> </v-btn>
+      <v-btn value="settings" @click="show_settings()"> <v-icon>mdi-cog</v-icon> <span>设置</span> </v-btn>
+      <v-btn value="next" @click='rendition.next()'> <v-icon>mdi-dots-horizontal-circle-outline</v-icon> <span>更多</span> </v-btn>
     </v-bottom-navigation>
 
     <v-bottom-sheet v-model="active_settings" contained persistent style="margin-bottom: 56px;" z-index="234">
@@ -39,21 +22,20 @@
     </v-bottom-sheet>
 
     <v-bottom-sheet v-model="active_toc" contained close-on-content-click style="margin-bottom: 56px;" z-index="234">
-      <book-toc :toc_items="toc_items" @click:select="click_toc"></book-toc>
+      <book-toc :toc_items="toc_items" @click:select="on_click_toc"></book-toc>
     </v-bottom-sheet>
 
     <!-- 阅读界面 -->
     <v-main class="pa-0 fill-height">
-          <div id="book-container">
+        <div id="book-container" class="fill-height">
             <div id="reader" class="fill-height"></div>
-          </div>
+        </div>
     </v-main>
 
   </v-app>
 </template>
 
 <script>
-import Book from 'epubjs'
 import FootBar from '../components/FootBar.vue'
 
 export default {
@@ -88,29 +70,43 @@ export default {
       this.active_settings = false;
       this.active_toc = !this.active_toc;
     },
-    click_toc: function (item) {
+    on_click_toc: function (item) {
       console.log(item);
       this.rendition.display(item.id);
     },
-    click_content: function (event) {
+    on_click_content: function (event) {
       const viewer = document.getElementById('reader');
       const width = viewer.offsetWidth;
       const x = event.clientX % viewer.offsetWidth;
       const y = event.clientY % viewer.offsetHeight;
-      this.show_click(x, y)
+      // this.show_click(x, y, width)
 
       if (x < width / 3) {
         // 点击左侧，往前翻页
+        console.log("prev page")
         this.rendition.prev();
       } else if (x > width * 2 / 3) {
         // 点击右侧，往后翻页
-        this.rendition.next();
+        console.log("next page")
+        this.rendition.next().then();
       } else {
+        // 点击中间，显示菜单
         this.active_menu = !this.active_menu;
       }
     },
-    show_click: function (x, y) {
-      console.log("click at", x, y)
+    on_keyup: function(e) {
+        const c = e.keyCode || e.which;
+        // Left & Up
+        if (c == 37 || c == 38) {
+            this.rendition.prev();
+        }
+        // Right & Down
+        if (c == 39 || c == 40) {
+            this.rendition.next();
+        }
+    },
+    show_click: function (x, y, width) {
+      console.log("click at", x, y, width)
       x = x - 10;
       y = y - 10;
       const dotDiv = document.createElement('div');
@@ -124,22 +120,55 @@ export default {
       setTimeout(() => {
         document.body.removeChild(dotDiv);
       }, 2000);
-    }
+    },
+    init_listeners: function() {
+        document.addEventListener('keyup', this.on_keyup, false);
+        this.rendition.on('keyup', this.on_keyup, false);
+        this.rendition.on('click', this.on_click_content, false);
+
+        var signals = ['click', 'selected', 'touchstart', 'touchend', 'touchmove'];
+        var signals = ["added", "attach", "attached", "axis", "changed", "detach", "displayed", "displayerror", "expand", "hidden", "layout", "linkClicked", "loaderror", "locationChanged", "markClicked", "openFailed", "orientationchange", "relocated", "removed", "rendered", "resize", "resized", "scroll", "scrolled", "selected", "selectedRange", "shown", "started", "updated", "writingMode", "mouseup", "mousedown", "mousemove", "click", "touchend", "touchstart", "touchmove"]
+        signals.forEach( sig => {
+            this.rendition.on(sig, (e) => {
+                this.alert_msg = sig;
+                console.log(sig, e);
+            }, false)
+        });
+
+    },
+    init_themes: function() {
+        this.rendition.themes.register("day", "themes.css");
+        this.rendition.themes.register("dark", "themes.css");
+        this.rendition.themes.register("night", "themes.css");
+        this.rendition.themes.register("brown", "themes.css");
+        this.rendition.themes.register("eyecare", "themes.css");
+
+        /*
+        this.rendition.themes.default({
+          h2: {
+            'font-size': '32px',
+            color: 'purple'
+          },
+          p: {
+            "margin": '10px'
+          }
+        });
+        */
+
+        this.rendition.themes.select('day');
+
+    },
   },
   mounted: function () {
-    this.book = new Book("/book2/");
+    this.book = ePub("/book/");
     this.rendition = this.book.renderTo("reader", {
       manager: "continuous",
       flow: "paginated",
       width: "100%",
       height: "100%",
-      // snap: true
-
+      //snap: true
     });
 
-    //var cfi = "Text/chapter175.html";
-    var cfi = "Text/part0010.xhtml";
-    this.rendition.display(cfi);
     this.book.loaded.metadata.then(metadata => {
       this.book_title = metadata.title;
     });
@@ -149,35 +178,20 @@ export default {
       this.toc_items = nav.toc
     });
 
-    this.rendition.on('click', this.click_content, false);
-    this.rendition.on('touchend', (e) => {
-        console.log("touch", e);
-    }, false);
+    window.rend = this.rendition
 
+    this.init_listeners();
 
-    this.rendition.themes.register("day", "themes.css");
-    this.rendition.themes.register("dark", "themes.css");
-    this.rendition.themes.register("night", "themes.css");
-    this.rendition.themes.register("brown", "themes.css");
-    this.rendition.themes.register("eyecare", "themes.css");
-
-    this.rendition.themes.default({
-      h2: {
-        'font-size': '32px',
-        color: 'purple'
-      },
-      p: {
-        "margin": '10px'
-      }
-    });
-
-    this.rendition.themes.select('eyecare');
-
+    this.book.ready.then( () => {
+        this.rendition.display();
+    })
+    this.init_themes();
 
   },
   data: () => ({
     book: null,
     book_title: "",
+    alert_msg: "x",
     rendition: null,
     auto_close: false,
     active_menu: true,
@@ -199,16 +213,6 @@ export default {
 </script>
 
 <style>
-#book-container {
-  height: 100%;
-  width: 100%;
-}
-
-#reader {
-  width: 100%;
-  height: 100%;
-}
-
 .dot {
   width: 20px;
   height: 20px;
@@ -217,10 +221,5 @@ export default {
   position: absolute;
   transition: opacity 1s ease-out;
   z-index: 999;
-}
-
-.bottom-panel {
-  margin-bottom: 56px;
-  z-index: 234;
 }
 </style>
